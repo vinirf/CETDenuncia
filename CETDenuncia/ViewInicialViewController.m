25d9d552b5b870220d256772a0e1c225a7c41b2e
Emerson Barros <emerson.sbarros@gmail.com>
2014-10-27 15:01:31 -0200

    Imagem pino carregamento

--- CETDenuncia/ViewInicialViewController.m
+++ CETDenuncia/ViewInicialViewController.m
@@ -60,7 +60,7 @@
     
     //Se for uma versão igual o maior do iOS8 requer autorização especial
     if ([self.locationManager respondsToSelector:@selector(requestAlwaysAuthorization)]) {
-        [self.locationManager requestAlwaysAuthorization];
+//        [self.locationManager requestAlwaysAuthorization];
     }
     [self.locationManager startUpdatingLocation];
     
